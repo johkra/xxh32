7faede0d957a36ef942090bde4d2cbb27792553e
Johannes Krampf <johannes.krampf@googlemail.com>
2019-02-04 21:22:55 +0100

    Make benches run on stable.

--- benches/lib.rs
+++ benches/lib.rs
@@ -1,27 +1,36 @@
-#![feature(test)]
+use bencher::Bencher;
+use bencher::{benchmark_group, benchmark_main};
+use xxh32::XXH32;
 
-extern crate test;
-extern crate xxh32;
+const BUFSIZE: usize = 100 * 1024;
 
-#[cfg(test)]
-mod tests {
-    use test::Bencher;
-    use xxh32::XXH32;
+fn bench_aligned(b: &mut Bencher) {
+    let mut buf = [0u8; BUFSIZE];
+    b.bytes = BUFSIZE as u64;
 
-    const BUFSIZE: usize = 100 * 1024 * 1024;
-
-    #[bench]
-    fn bench_hashing(b: &mut Bencher) {
-        let mut buf = vec![0u8; BUFSIZE];
-        b.bytes = BUFSIZE as u64;
-
-        for (i, el) in buf.iter_mut().enumerate() {
-            *el = i as u8;
-        }
-        b.iter(|| {
-            let mut xxh32 = XXH32::default();
-            xxh32.write(&buf);
-            xxh32.finish()
-        })
+    for (i, el) in buf.iter_mut().enumerate() {
+        *el = i as u8;
     }
+    b.iter(|| {
+        let mut xxh32 = XXH32::default();
+        xxh32.write(&buf);
+        xxh32.finish()
+    })
 }
+
+fn bench_unaligned(b: &mut Bencher) {
+    let mut buf = [0u8; BUFSIZE + 1024];
+    b.bytes = BUFSIZE as u64;
+
+    for (i, el) in buf.iter_mut().enumerate() {
+        *el = i as u8;
+    }
+    b.iter(|| {
+        let mut xxh32 = XXH32::default();
+        xxh32.write(&buf[1..1 + BUFSIZE]);
+        xxh32.finish()
+    })
+}
+
+benchmark_group!(benches, bench_aligned, bench_unaligned);
+benchmark_main!(benches);
