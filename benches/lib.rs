82bbfa1f6ce6c53a333acfee1cd61fea6f4b8dd5
Johannes Krampf <johannes.krampf@googlemail.com>
2019-02-09 13:21:20 +0100

    Add more benchmarks

--- benches/lib.rs
+++ benches/lib.rs
@@ -32,5 +32,77 @@ fn bench_unaligned(b: &mut Bencher) {
     })
 }
 
-benchmark_group!(benches, bench_aligned, bench_unaligned);
+fn bench_small_updates1(b: &mut Bencher) {
+    let mut buf = [0u8; BUFSIZE];
+    b.bytes = BUFSIZE as u64;
+
+    for (i, el) in buf.iter_mut().enumerate() {
+        *el = i as u8;
+    }
+    b.iter(|| {
+        let mut xxh32 = XXH32::default();
+        for b in buf.iter() {
+            xxh32.write(&[*b]);
+        }
+        xxh32.finish()
+    })
+}
+
+fn bench_small_updates8(b: &mut Bencher) {
+    let mut buf = [0u8; BUFSIZE];
+    b.bytes = BUFSIZE as u64;
+
+    for (i, el) in buf.iter_mut().enumerate() {
+        *el = i as u8;
+    }
+    b.iter(|| {
+        let mut xxh32 = XXH32::default();
+        for chunk in buf.chunks_exact(8) {
+            xxh32.write(chunk);
+        }
+        xxh32.finish()
+    })
+}
+
+fn bench_small_updates16(b: &mut Bencher) {
+    let mut buf = [0u8; BUFSIZE];
+    b.bytes = BUFSIZE as u64;
+
+    for (i, el) in buf.iter_mut().enumerate() {
+        *el = i as u8;
+    }
+    b.iter(|| {
+        let mut xxh32 = XXH32::default();
+        for chunk in buf.chunks_exact(16) {
+            xxh32.write(chunk);
+        }
+        xxh32.finish()
+    })
+}
+
+fn bench_small_updates100(b: &mut Bencher) {
+    let mut buf = [0u8; BUFSIZE];
+    b.bytes = BUFSIZE as u64;
+
+    for (i, el) in buf.iter_mut().enumerate() {
+        *el = i as u8;
+    }
+    b.iter(|| {
+        let mut xxh32 = XXH32::default();
+        for chunk in buf.chunks_exact(100) {
+            xxh32.write(chunk);
+        }
+        xxh32.finish()
+    })
+}
+
+benchmark_group!(
+    benches,
+    bench_aligned,
+    bench_unaligned,
+    bench_small_updates1,
+    bench_small_updates8,
+    bench_small_updates16,
+    bench_small_updates100
+);
 benchmark_main!(benches);
